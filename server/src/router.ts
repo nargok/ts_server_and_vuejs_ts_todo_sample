4a936f7519819d051038124d4f5c108a00ff85ed
nargok <nargok@gmail.com>
2021-10-24 13:07:27 +0900

    setup nodemon

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,7 +3,19 @@ import { Router } from "express";
 export const createRouter = () => {
   const router = Router();
 
-  router.get("/helloworld", (req, res) => {
+  router.get("/", (req, res) => {
+    res.status(200).send({ message: "hello, world" });
+  });
+
+  router.put("/", (req, res) => {
+    res.status(200).send({ message: "hello, world" });
+  });
+
+  router.post("/:taskID", (req, res) => {
+    res.status(200).send({ message: "hello, world" });
+  });
+
+  router.delete("/:taskID", (req, res) => {
     res.status(200).send({ message: "hello, world" });
   });
 
