a5c3b73ee28dd2ce57065b9350c470d06451c5d2
nargok <nargok@gmail.com>
2021-10-24 13:37:22 +0900

    task api

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,22 +1,58 @@
 import { Router } from "express";
+import { DBAccessor } from "./dbAccessor";
+
+const dbAccessor = new DBAccessor();
 
 export const createRouter = () => {
   const router = Router();
 
-  router.get("/", (req, res) => {
-    res.status(200).send({ message: "hello, world" });
+  router.get("/", async (req, res) => {
+    try {
+      const resBody = await dbAccessor.get();
+      res.status(200).send({ message: "get success", resBody });
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ message: "get failded" });
+    }
   });
 
-  router.put("/", (req, res) => {
-    res.status(200).send({ message: "hello, world" });
+  router.post("/", async (req, res) => {
+    try {
+      if (!req.body.title) {
+        res.status(400).send({ message: "title required" });
+      }
+      await dbAccessor.create(req.body.title);
+      res.status(200).send({ message: "create success" });
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ message: "create failded" });
+    }
   });
 
-  router.post("/:taskID", (req, res) => {
-    res.status(200).send({ message: "hello, world" });
+  router.put("/:taskID", async (req, res) => {
+    try {
+      if (!req.body) {
+        res.status(400).send({ message: "body required" });
+      }
+      await dbAccessor.update({ uuid: req.params.taskID, ...req.body });
+      res.status(200).send({ message: "update success" });
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ message: "update failded" });
+    }
   });
 
-  router.delete("/:taskID", (req, res) => {
-    res.status(200).send({ message: "hello, world" });
+  router.delete("/:taskID", async (req, res) => {
+    try {
+      if (!req.body) {
+        res.status(400).send({ message: "body required" });
+      }
+      await dbAccessor.delete({ uuid: req.params.taskID });
+      res.status(200).send({ message: "delete success" });
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ message: "delete failded" });
+    }
   });
 
   return router;
